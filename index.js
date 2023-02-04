4491d2bfe6d2c2cfdb19d58fafa7e4aff268b511
chaitanya sai <chaitanyasai.m.5@gmail.com>
2023-02-04 20:59:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(express.static(__dirname+ "/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+  const firstName=req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members:[
+      {
+      email_address:email,
+      status: "subscribed",
+      merge_fields:{
+        FNAME: firstName,
+        LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/4d818ddfee";
+  const options={
+    method: "POST",
+    auth: "chaitu:11cb0d600788e727f2d2a38860eb42f1-us21"
+  }
+
+  const request=https.request(url,options,function(response){
+
+    if(response.statusCode===200){
+      res.sendFile(__dirname + "/index.html");
+    }else{
+      res.sendFile(__dirname+"/failure.html");
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+request.write(jsonData);
+request.end();
+
+  });
+
+
+  app.post("/failure",function(req,res){
+    res.redirect("/");
+  })
+
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server started");
+})
+
+
+// Api Key
+// e1849168d3ab34736ae661a3c9faa26f-us21
+
+// List ID
+//4d818ddfee
---
+++ public/index.js
@@ -0,0 +1,7 @@
+var typed=new Typed('#home-heading',{
+    strings:['Pre-Book your Table and Order Your items before you reach the Restaurant.'],
+    typeSpeed:40
+})
+AOS.init({
+    duration: 2000
+})
